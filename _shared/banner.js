a65c4c70da26e61167cf4d6dbbefb02b5772cbac
Brandon van den Bosch <brandonvandenbosch@hotmail.com>
2024-01-11 11:28:38 +0100

    afbeelding met voorbeeld test voor header toegevoegd

---
+++ _shared/banner.js
@@ -0,0 +1,9 @@
+// JS om banner op te halen
+fetch('/_shared/banner.html')
+    .then(res => res.text())
+    .then(text => {
+        let oldelem = document.querySelector("script#banner");
+        let newelem = document.createElement("div");
+        newelem.innerHTML = text;
+        oldelem.parentNode.replaceChild(newelem, oldelem);
+    })
