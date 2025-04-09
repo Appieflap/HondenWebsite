eac5d6673bbd4ccbebd8ffb61de6bb0d513e6cb7
Sharon <sharonpeeterss@hotmail.com>
2025-04-09 17:50:47 +0200

    fix

--- shared/banner/banner.js
+++
@@ -1,7 +0,0 @@
-// JS om banner op te halen
-fetch('/shared/banner/banner.html')
-.then(response => response.text())
-.then(html => {
-    document.getElementById('banner-container').innerHTML = html;
-})
-.catch(error => console.error('Error handeling banner:', error));
