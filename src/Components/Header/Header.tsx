f9dcb16f19e329b49495df91ab1b032bd6fb9845
Brandon <brandonvandenbosch@hotmail.com>
2026-01-04 16:47:40 +0100

    Google link bijgewerkt, zodat deze verwijst naar vrolijkstaartje ipv enkel het adres.

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -9,7 +9,7 @@ export const Header: FC = () => {
         <Styled.Container>
             <Styled.ContentContainer>
                 <a
-                    href="https://www.google.com/maps/place/Hofstraat+27A,+6161+AP+Geleen"
+                    href="https://www.google.com/maps/place/Vrolijk+Staartje+hondentrainer+%26+trimsalon/@50.963084,5.8180545,16.99z/data=!4m15!1m8!3m7!1s0x47c0c73af39dbbf9:0xa37f4a21fc316f8b!2sHofstraat+27A,+6161+AP+Geleen!3b1!8m2!3d50.9630812!4d5.8180574!16s%2Fg%2F11c5j3vtnp!3m5!1s0x47c0c746d02391d3:0x3e9830cfcf6c37b9!8m2!3d50.9630812!4d5.8180574!16s%2Fg%2F11x8xj159_"
                     target="_blank"
                     rel="noopener noreferrer"
                     style={{ textDecoration: "none", color: "inherit" }}
